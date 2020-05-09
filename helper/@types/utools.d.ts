e3ac9ea507fbdb82a01742d25da8245d32f164dd
mohuishou <1@lailin.xyz>
2020-05-10 02:00:54 +0800

    feat(helper): 适配 utools v1.0.0

--- helper/@types/utools.d.ts
+++ helper/@types/utools.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="electron.d.ts" />
+
 /**
  * @description 文档链接: https://u.tools/docs/developer/api.html
  */
@@ -14,7 +16,7 @@ export interface UTools {
    * @description 每当插件从后台进入到前台时，uTools将会主动调用这个方法
    * @param cb 回调函数
    */
-  onPluginEnter(cb: Function): void;
+  onPluginEnter(cb: (params: onPluginEnterCBParams) => {}): void;
 
   /**
    * @description 每当插件从前台进入到后台时，uTools将会主动调用这个方法
@@ -34,14 +36,12 @@ export interface UTools {
    */
   onDbPull(cb: Function): void;
 
-  // 数据库 api
-  db: DB;
-
   // 窗口交互 API
   /**
    * @description 执行该方法将会隐藏uTools主窗口，包括此时正在主窗口运行的插件，分离的插件不会被隐藏。
+   * @param isRestorePreWindow 是否焦点回归到前面的活动窗口，默认 true
    */
-  hideMainWindow(): Boolean;
+  hideMainWindow(isRestorePreWindow?: Boolean): Boolean;
 
   /**
    * @description 执行该方法将会显示uTools主窗口，包括此时正在主窗口运行的插件。
@@ -100,9 +100,59 @@ export interface UTools {
   /**
    * 该方法可以携带数据，跳转到另一个插件进行处理，如果用户未安装对应的插件，uTools会弹出提醒并引导进入插件中心下载。
    * @param cmd 插件关键词
-   * @param content
+   * @param payload
    */
-  redirect(cmd: string, content: string | any): Boolean;
+  redirect(cmd: string, payload: string | any): Boolean;
+
+  /**
+   * 弹出文件选择框
+   * @param options OpenDialogSyncOptions
+   */
+  showOpenDialog(
+    options: Electron.OpenDialogSyncOptions
+  ): Array<string> | undefined;
+
+  /**
+   * 打开文件保存框
+   * @param options SaveDialogSyncOptions
+   */
+  showSaveDialog(options: Electron.SaveDialogSyncOptions): string | undefined;
+
+  /**
+   * 弹出消息框
+   */
+  showMessageBox(options: Electron.MessageBoxSyncOptions): Number;
+
+  /**
+   * 插件页面中查找内容
+   * @param text 要搜索的内容(必填):Ubrowser;
+   * @param options
+   */
+  findInPage(text: string, options?: Electron.FindInPageOptions): void;
+
+  /**
+   * 停止插件页面中查找
+   * @param action "clearSelection" | "keepSelection" | "activateSelection", 默认 "clearSelection"
+   */
+  stopFindInPage(
+    action?: "clearSelection" | "keepSelection" | "activateSelection"
+  ): void;
+
+  /**
+   * 原生拖拽文件到其他窗口
+   * @param file 文件路径 或 文件路径集合
+   */
+  startDrag(file: string | Array<string>): void;
+
+  /**
+   * 创建浏览器窗口
+   * @param url  相对路径的html文件 例如: test.html?param=xxx
+   * @param options 注意: preload 需配置相对位置
+   */
+  createBrowserWindow(
+    url: string,
+    options: Electron.BrowserWindowConstructorOptions
+  ): void;
 
   // 动态增减
   /**
@@ -122,24 +172,105 @@ export interface UTools {
    */
   removeFeature(code: string): Boolean;
 
-  // 其他
+  // 工具
+
+  /**
+   * 屏幕取色
+   * @param cb 取色结束回调
+   */
+  screenColorPick(cb: (options: screenColorPickCBOptions) => {}): void;
+
+  /**
+   * 屏幕截图
+   * @param cb 截图结束回调
+   */
+  screenCapture(cb: (img: string) => {}): void;
+
+  // 模拟
+
+  /**
+   * 模拟键盘按键
+   * @param key 键值
+   * @param modifier 功能键
+   */
+  simulateKeyboardTap(key: string, ...modifier: string[]): void;
+
+  /**
+   * 模拟鼠标移动
+   */
+  simulateMouseMove(x: Number, y: Number): void;
+
+  /**
+   * 模拟鼠标左键单击
+   */
+  simulateMouseClick(x: Number, y: Number): void;
+
+  /**
+   * 模拟鼠标右键单击
+   */
+  simulateMouseRightClick(x: Number, y: Number): void;
+
+  /**
+   * 模拟鼠标双击
+   */
+  simulateMouseDoubleClick(x: Number, y: Number): void;
+
+  // 屏幕 TODO: 待完善
+
+  // 复制
+
+  /**
+   * 复制文件到系统剪贴板
+   * @param file
+   */
+  copyFile(file: string | Array<string>): Boolean;
+
+  /**
+   * 复制图片到系统剪贴板
+   * @param img
+   */
+  copyImage(img: string | Buffer): Boolean;
+
+  /**
+   * 复制文本
+   * @param text
+   */
+  copyText(text: string): Boolean;
+
+  // 系统
   /**
    * @description 执行该方法将会弹出一个系统通知。
    * @param body 显示的内容
-   * @param code 用户点击系统通知时，uTools将会使用此`code`进入插件
-   * @param silent 是否播放声音
    */
-  showNotification(body: string, code?: string, silent?: Boolean): Boolean;
+  showNotification(body: string): Boolean;
 
   /**
-   * @description 该方法只适用于在macOS下执行，用于判断uTools是否拥有辅助权限，如果没有可以调用API方法requestPrivilege请求
+   * 系统默认方式打开给定的文件
+   * @param path 文件绝对路径
    */
-  isHadPrivilege(): Boolean;
+  shellOpenItem(path: string): void;
 
   /**
-   * @description  该方法只适用于在macOS下执行，该方法调用后会弹出窗口向用户申请辅助权限。
+   * 系统文件管理器中显示给定的文件
+   * @param path 文件绝对路径
    */
-  requestPrivilege(): Boolean;
+  shellShowItemInFolder(path: string): void;
+
+  /**
+   * 系统默认的协议打开URL
+   * @param url
+   */
+  shellOpenExternal(url: string): void;
+
+  /**
+   * 播放哔哔声
+   */
+  shellBeep(): void;
+
+  /**
+   * @description 获取本地机器唯一ID，可以根据此值区分同一用户的不同设备
+   */
+  getLocalId(): string;
 
   /**
    * @description 你可以通过名称请求以下的路径
@@ -160,34 +291,41 @@ export interface UTools {
    * @param name
    */
   getPath(name: PathName): string;
-  /**
-   * @description 复制文件或文件夹到剪贴板
-   * @param paths
-   */
-  copyFile(paths: string | Array<string>): Boolean;
-  /**
-   * @description 复制图片到剪贴板
-   * @param buffer
-   */
-  copyImage(buffer: ArrayBuffer): Boolean;
-  /**
-   * @description 屏幕取色
-   * @param cb
-   */
-  screenColorPick(cb: Function): void;
-  /**
-   * @description 获取当前浏览器URL (呼出uTools前的活动窗口)
-   */
-  getCurrentBrowserUrl(): string;
-  /**
-   * @description 获取本地机器唯一ID，可以根据此值区分同一用户的不同设备
-   */
-  getLocalId(): string;
 
   /**
-   * robotjs是一个优秀的自动化库，可以实现控制鼠标、键盘、获取屏幕信息等功能，可以实现很多自动化操作。
+   * @description 获取当前浏览器URL (呼出uTools前的活动窗口):Ubrowser;
    */
-  robot: any;
+  getCurrentBrowserUrl(): string;
+
+  /**
+   * 获取当前文件管理器路径(linux 不支持)，呼出uTools前的活动窗口为资源管理器才能获取
+   */
+  getCurrentFolderPath(): string;
+
+  isMacOs(): void;
+
+  isWindows(): void;
+
+  isLinux(): void;
+
+  /**
+   * @description 该方法只适用于在macOS下执行，用于判断uTools是否拥有辅助权限，如果没有可以调用API方法requestPrivilege请求
+   */
+  isHadPrivilege(): Boolean;
+
+  /**
+   * @description  该方法只适用于在macOS下执行，该方法调用后会弹出窗口向用户申请辅助权限。
+   */
+  requestPrivilege(): Boolean;
+
+  // 数据库 api
+  db: DB;
+
+  // ubrowser api
+  ubrowser: Ubrowser;
+
+  getIdleUBrowsers(): Array<Object>;
+  setUBrowserProxy(config: Object): boolean;
 }
 
 export type PathName =
@@ -253,6 +391,11 @@ export interface DB {
   allDocs<T = any>(id?: string | Array<string>): DBRes<T>[];
 }
 
+export interface screenColorPickCBOptions {
+  hex: string;
+  rgb: string;
+}
+
 export interface DBItem<T> {
   _id: string;
   _rev?: string;
@@ -307,7 +450,7 @@ export interface TplFeatureArgsEnter {
  * @param action action
  * @param word 搜索的字符创
  * @param cb 回调函数，可以继续设置列表项目
- * @description 子输入框内容变化时被调用 可选 (未设置则无搜索)
+ * @description 子输入框内容变化时被调用 可选 (未设置则无搜索):Ubrowser;
  */
 export interface TplFeatureArgsSearch {
   (action: Action, word: string, cb: CallbackSetList): void;
@@ -348,4 +491,77 @@ export interface TplFeature {
  */
 export interface TemplatePlugin {
   [index: string]: TplFeature;
+}
+
+interface onPluginEnterCBParams {
+  code: string; // plugin.json 配置的 feature.code
+  type: string; // plugin.json 配置的 feature.cmd.code
+  payload: string; // feature.cmd.type 对应匹配的数据
+  optional?: Array<any>; // 存在多个匹配时的可选匹配类型和数据 [{ type, payload }]
+}
+
+// ubrowser 相关
+/**
+ * 请查看 https://u.tools/docs/developer/ubrowser.html
+ */
+export interface Ubrowser {
+  useragent(userAgent: string): Ubrowser;
+  goto(url: string, headers?: Object): Ubrowser;
+  goto(mdText: string, title?: string): Ubrowser;
+  viewport(width: Number, height: Number): Ubrowser;
+  hide(): Ubrowser;
+  show(): Ubrowser;
+  css(cssCode: string): Ubrowser;
+  press(key: string, ...modifier: string[]): Ubrowser;
+  paste(text?: string): Ubrowser;
+  screenshot(arg?: string | Object, savePath?: string): Ubrowser;
+  pdf(options?: Object, savePath?: string): Ubrowser;
+  device(arg: string | Object): Ubrowser;
+  cookies(name?: string): Ubrowser;
+  setCookies(name: string, value: string): Ubrowser;
+  setCookies(cookies: Array<Object>): Ubrowser;
+  removeCookies(name: string): Ubrowser;
+  clearCookies(url?: string): Ubrowser;
+  devTools(mode?: "right" | "bottom" | "undocked" | "detach"): Ubrowser;
+  evaluate(func: Function, ...params: Array<any>): Ubrowser;
+  wait(ms: Number): Ubrowser;
+  wait(selector: string, timeout?: Number): Ubrowser;
+  wait(func: Function, timeout: Number, ...params: Array<any>): Ubrowser;
+  when(selector: string): Ubrowser;
+  when(func: Function, ...params: Array<any>): Ubrowser;
+  end(): Ubrowser;
+  click(selector: string): Ubrowser;
+  mousedown(selector: string): Ubrowser;
+  mouseup(selector: string): Ubrowser;
+  file(selector: string, payload: string | Array<string> | Buffer): Ubrowser;
+  value(selector: string, val: string): Ubrowser;
+  check(selector: string, checked: Boolean): Ubrowser;
+  focus(selector: string): Ubrowser;
+  scroll(selector: string): Ubrowser;
+  scroll(y: Number): Ubrowser;
+  scroll(x: Number, y: Number): Ubrowser;
+  run(ubrowserId: Number): Promise<Ubrowser>;
+  run(options: UbrowserRunOptions): Promise<Ubrowser>;
+}
+
+export interface UbrowserRunOptions {
+  show: Boolean; //(可选)
+  width: Number; //(可选) 宽度
+  height: Number; //(可选) 高度
+  x: Number; //(可选)
+  y: Number; //(可选)
+  center: Boolean; //(可选)
+  minWidth: Number; //(可选) 窗口的最小宽度, 默认值为
+  minHeight: Number; //(可选) 窗口的最小高度. 默认值为
+  maxWidth: Number; //(可选) 窗口的最大宽度,
+  maxHeight: Number; //(可选) 窗口的最大高度,
+  resizable: Boolean; //(可选) 窗口是否可以改变尺寸,
+  movable: Boolean; //(可选) 窗口是否可以移动. 在 Linux 中无效。 默认值为
+  minimizable: Boolean; //(可选) 窗口是否可以最小化. 在 Linux 中无效。 默认值为
+  maximizable: Boolean; //(可选) 窗口是否可以最大化动. 在 Linux 中无效。 默认值为
+  alwaysOnTop: Boolean; //(可选) 窗口是否永远置顶。
+  fullscreen: Boolean; //(可选) 窗口是否全屏.
+  fullscreenable: Boolean; //(可选) 窗口是否可以进入全屏状态，默认值为
+  enableLargerThanScreen: Boolean; //(可选) 是否允许改变窗口的大小使之大于屏幕的尺寸. 仅适用于 macOS，因为其它操作系统默认允许 大于屏幕的窗口。 默认值为
+  opacity: Number; //(可选) 设置窗口初始的不透明度, 介于 0.0 (完全透明) 和 1.0 (完全不透明) 之间。仅支持 Windows 和
 }

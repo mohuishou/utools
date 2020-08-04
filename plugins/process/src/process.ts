cde6e0dbe5b5f77da64f8fbad994b9fe7282d887
mohuishou <1@lailin.xyz>
2020-08-04 14:21:03 +0800

    feat: 添加图标缓存，优化进程获取性能

--- plugins/process/src/process.ts
+++ plugins/process/src/process.ts
@@ -1,4 +1,5 @@
 import { Plugin, IListItem, ListItem } from "utools-helper";
+import pidusage = require("pidusage");
 import psList = require("ps-list");
 const fileIcon = require("file-icon");
 
@@ -11,31 +12,34 @@ export class Process implements Plugin {
 
   async search(word: string): Promise<IListItem[]> {
     let processes = await psList();
-    processes = processes.sort((a, b) => -a.cpu + b.cpu);
 
     let words = word.toLowerCase().split(/\s+/g);
     words.forEach((w) => {
       processes = processes.filter((p) => p.name.toLowerCase().includes(w));
     });
 
-    processes = processes.slice(1, 20);
+    processes = processes.sort((a, b) => -a.cpu + b.cpu).slice(1, 20);
 
     return await Promise.all(
       processes.map(
         async (p): Promise<IListItem> => {
-          let icon;
-          try {
-            let id = p.ppid === 1 ? p.pid : p.ppid;
-            icon =
-              "data:image/png;base64," +
-              Buffer.from(await fileIcon.buffer(id)).toString("base64");
-          } catch (error) {
-            console.log(error);
-            icon = "system.png";
+          let icon = localStorage.getItem(p.name);
+          if (!icon) {
+            try {
+              let id = p.ppid === 1 ? p.pid : p.ppid;
+              icon =
+                "data:image/png;base64," +
+                Buffer.from(await fileIcon.buffer(id)).toString("base64");
+            } catch (error) {
+              console.log(error);
+              icon = "system.png";
+            }
+            localStorage.setItem(p.name, icon);
           }
+
           return {
             title: p.name,
-            description: `cpu: ${p.cpu}%, mem: ${p.memory}%, cmd: ${p.cmd}`,
+            description: `cpu: ${p.cpu}%, mem: ${p.memory}, cmd: ${p.cmd}`,
             icon: icon,
             data: p,
           };
